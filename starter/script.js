e36314c3144ef0b7b8c82e4771387e3a165d7c4b
Efftsh <rising@gmail.com>
2026-04-06 14:02:50 +0800

    How Passing Argument Works: Value vs Reference Session

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
+/*//Default Parameter
+//////////////////////
 const bookings = [];
-
 const createBooking = function (
   flightNumber = 'A302',
   passengerNumber = 1,
@@ -20,3 +21,33 @@ createBooking(undefined, 10);
 createBooking(undefined, undefined, 500);
 createBooking();
 console.log(bookings);
+ */
+
+/* //How Passing Argument Works: Value vs Reference
+//////////////////////
+const flight = 'LH234';
+const iffat = {
+  name: 'Iffat Ashizam',
+  passport: 2348439832,
+};
+
+const checkIn = function (flightNumber, passenger) {
+  flightNumber = 'LH999';
+  passenger.name = 'Mr ' + passenger.name;
+
+  if (passenger.passport === 2348439832) {
+    alert('Check in');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+// checkIn(flight, iffat);
+// console.log(flight);
+// console.log(iffat);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+newPassport(iffat);
+checkIn(flight, iffat); */
