5105fb9bcc8ac4a79d9088b039830ee914faee29
Efftsh <rising@gmail.com>
2026-04-10 17:29:25 +0800

    The End of A Closer Look At Functions

--- challenge.js
+++ challenge.js
@@ -53,7 +53,7 @@ const greeter = greeting('Yahuw');
 
 transformer(greeter('iffat'), firstWordUpper); */
 
-//Call,Apply, and Bind Function Method
+/* //Call,Apply, and Bind Function Method
 const person1 = {
   name: 'John Doe',
   birthYear: 2002,
@@ -109,8 +109,9 @@ const sumA = sum.bind(null, 10);
 console.log(sumA(40));
 console.log(sumA(20));
 console.log(sumA(55));
+ */
 
-/* 
+/* Challenge 1
 Let's build a simple poll app!
 
 A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
@@ -139,7 +140,7 @@ BONUS TEST DATA 1: [5, 2, 3]
 BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
-*/
+
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
@@ -186,3 +187,22 @@ poll.displayResult.call(bonus2, 'array');
 poll.displayResult.call(bonus2, 'sTrIng');
 const pollBtn = document.querySelector('.poll');
 pollBtn.addEventListener('click', poll.registerNewAnswer.bind(poll));
+*/
+
+/* Coding Challenge #2
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
