8c0e64cb251c0cbe238e38efa0f06a6b6ef720bd
Efftsh <rising@gmail.com>
2026-04-10 17:12:50 +0800

    More Closure Example session

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ runOnce();
 console.log(privateNum);
  */
 
-//Closure
+/* //Closure
 //////////////////////
 const secureBooking = function () {
   let passengerCount = 0;
@@ -207,4 +207,41 @@ const secureBooking = function () {
 const booker = secureBooking();
 booker();
 booker();
-booker();
+booker() 
+*/
+
+/* //Moren Closure Example
+//////////////////////
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+console.dir(f);
+h();
+f();
+console.dir(f);
+
+const boardPassenger = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passenger`);
+    console.log(`There are 3 groups, each with ${perGroup} passenger`);
+  }, wait * 1000);
+
+  console.log(`Will start bording in ${wait} seconds`);
+};
+
+boardPassenger(180, 3);
+ */
